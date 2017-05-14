f28f07bacb0a9db281b93422e677c77bd1bd1d14
Jared Murphy <jaredcmurphy@gmail.com>
2017-05-13 21:27:30 -0400

    posts show working

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "posts#index"
   get "/posts", to: "posts#index"
   get "/posts/new", to: "posts#new"
-  get "/posts/:id", to: "posts#show"
+  get "/posts/:id", to: "posts#show", as: :post
   post "/posts", to: "posts#create"
 end
 
